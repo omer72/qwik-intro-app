59c5a1cf2d5678749b4cb4b44d23f560a319edcb
Omer Etrog <omer72@gmail.com>
2024-03-28 21:35:31 +0200

    learn

--- src/routes/projector-ocntext-id.ts
+++ src/routes/projector-ocntext-id.ts
@@ -1,4 +1,9 @@
 import type { Signal} from "@builder.io/qwik";
 import { createContextId } from "@builder.io/qwik";
 
-export const projectorContextId = createContextId<Signal<string>>('ProjectorContext');
+export interface ProjectorContextSignal{
+    typeValue:Signal<string>;
+    colorSignal:Signal<string>;
+}
+
+export const projectorContextId = createContextId<ProjectorContextSignal>('ProjectorContext');
